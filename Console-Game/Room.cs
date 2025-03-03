20940c3a3a88e21238d208b4b62c2f5f762a0332
rubberduckster <mcrubygirl@hotmail.com>
2025-03-03 14:02:04 +0100

    Add project files.

---
+++ Console-Game/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Game
+{
+    public class Room
+    {
+        public string description { get; set; }
+        public Room()
+        { 
+        }
+    }
+}
